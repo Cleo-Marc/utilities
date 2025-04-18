575e6c5fa5ebdfaf2167dc980e505c9e2d2365e4
Tony Jinks <tony.jinks@gmail.com>
2025-04-18 10:37:24 +0100

    Added release folder

---
+++ PdfGen/release/README.txt
@@ -0,0 +1,6 @@
+To register the PdfGen COM object:
+
+1. Start a DOS command prompt running as Administrator
+2. Cd to the directory containing the PdgGen DLLs (and this README)
+3. Run the register.bat script
+4. Ignore annoying RegAsm warning
---
+++ PdfGen/release/register.bat
@@ -0,0 +1,6 @@
+:: Notes: 
+::   1. Requires admin privileges
+::   2. Must be run from the PdfGen release directory
+::   3. Ignore regasm warning
+
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\regasm.exe pdfgenwrapper.dll /codebase "%cd%"
